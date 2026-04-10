7aecf91570ddae65b46741f8bc0cdba7b4e473c6
unknown <G00471112@atu.ie>
2026-04-10 18:10:37 +0000

    Implement Chicken category page with API integration and recipe card UI

--- src/app/chicken/chicken.page.ts
+++ src/app/chicken/chicken.page.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonCard, IonCardHeader, IonCardTitle, IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-chicken',
   templateUrl: './chicken.page.html',
   styleUrls: ['./chicken.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonCard, IonCardHeader, IonCardTitle, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class ChickenPage implements OnInit {
 
-  constructor() { }
+  recipes: any[] = [];
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get('https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken').subscribe((data: any) => {
+      this.recipes = data.meals;
+      console.log(this.recipes);
+    });
   }
 
 }
