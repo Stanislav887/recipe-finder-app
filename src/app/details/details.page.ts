2ebbcfb6b0c1b546a665600cbc186299a3e7d5c9
unknown <G00471112@atu.ie>
2026-04-10 21:53:43 +0000

    Implement recipe details page with API lookup by ID and dynamic ingredients list display

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -3,18 +3,21 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonList, IonItem, IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.page.html',
   styleUrls: ['./details.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonList, IonItem, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class DetailsPage implements OnInit {
 
   meal: any;
+
+  ingredients: any[] = [];
 
   constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
@@ -23,6 +26,17 @@ export class DetailsPage implements OnInit {
 
     this.http.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`).subscribe((data: any) => {
       this.meal = data.meals[0];
+
+      this.ingredients = [];
+
+      for (let i = 1; i <= 20; i++) {
+        const ing = this.meal['strIngredient' + i];
+        const meas = this.meal['strMeasure' + i];
+
+        if (ing) {
+          this.ingredients.push(meas + ' ' + ing);
+        }
+      }
     });
   }
 
