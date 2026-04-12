37061a804d7a885bbccf224c218b728af2ecdf66
unknown <G00471112@atu.ie>
2026-04-12 17:12:48 +0000

    Refactor seafood page to use RecipeService

--- src/app/seafood/seafood.page.ts
+++ src/app/seafood/seafood.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import { RecipeService } from '../services/recipe.service';
 import { Router } from '@angular/router';
 import { IonButtons, IonBackButton, IonCard, IonCardHeader, IonCardTitle, IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
@@ -16,10 +16,10 @@ export class SeafoodPage implements OnInit {
 
   recipes: any[] = [];
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private recipeService: RecipeService, private router: Router) { }
 
   ngOnInit() {
-    this.http.get('https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood').subscribe((data: any) => {
+    this.recipeService.getMealsByCategory('Seafood').subscribe((data: any) => {
       this.recipes = data.meals;
       console.log(this.recipes);
     });
