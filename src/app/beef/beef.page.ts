754b75faaefa367f3ef307d2229622154a6427f4
unknown <G00471112@atu.ie>
2026-04-12 16:57:33 +0000

    Refactor beef page to use RecipeService and clean routing

--- src/app/beef/beef.page.ts
+++ src/app/beef/beef.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import { RecipeService } from '../services/recipe.service';
 import { Router } from '@angular/router';
 import { IonButtons, IonBackButton, IonCard, IonCardHeader, IonCardTitle, IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
@@ -16,10 +16,10 @@ export class BeefPage implements OnInit {
 
   recipes: any[] = [];
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private recipeService: RecipeService, private router: Router) { }
 
   ngOnInit() {
-    this.http.get('https://www.themealdb.com/api/json/v1/1/filter.php?i=beef').subscribe((data: any) => {
+    this.recipeService.getMealsByCategory('Beef').subscribe((data: any) => {
       this.recipes = data.meals;
     });
   }
