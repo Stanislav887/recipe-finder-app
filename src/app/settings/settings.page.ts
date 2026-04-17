717c080dd7b088ca9b44486b75ac06931f6135ce
unknown <G00471112@atu.ie>
2026-04-17 19:42:47 +0000

    Add Ionic Storage persistence for showImages setting with load and toggle sync in SettingsPage

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonItem, IonLabel, IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-settings',
@@ -12,8 +13,18 @@ import { IonItem, IonLabel, IonContent, IonHeader, IonTitle, IonToolbar } from '
 })
 export class SettingsPage implements OnInit {
 
-  constructor() { }
+  showImages: boolean = true;
 
+  constructor(private storage: Storage) { }
+
+  async toggleChanged() {
+    await this.storage.set('showImages', this.showImages);
+  }
+
+  async ionViewWillEnter() {
+    await this.storage.create();
+    this.showImages = await this.storage.get('showImages') ?? true;
+  }
   ngOnInit() {
   }
 
