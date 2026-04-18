ad431cde75a5113dcf72c3e15b22e449ac79046e
unknown <G00471112@atu.ie>
2026-04-18 22:23:21 +0000

    Add and style back button in Settings page toolbar for navigation to Home page

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -2,14 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Device } from '@capacitor/device';
-import { IonCardContent, IonCard, IonCardHeader, IonCardTitle, IonButton, IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonButtons, IonBackButton, IonCardContent, IonCard, IonCardHeader, IonCardTitle, IonButton, IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss'],
   standalone: true,
-  imports: [IonCardContent, IonCard, IonCardHeader, IonCardTitle, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButtons, IonBackButton, IonCardContent, IonCard, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class SettingsPage implements OnInit {
 
