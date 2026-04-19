dbabefc4198836f636e16b0422c5a7876c992470
unknown <G00471112@atu.ie>
2026-04-19 12:46:32 +0000

    Improve Favourites page UI with styled card layout, consistent background, image formatting, and empty state display

--- src/app/favourites/favourites.page.ts
+++ src/app/favourites/favourites.page.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Storage } from '@ionic/storage-angular';
 import { RecipeService } from '../services/recipe.service';
-import { IonButton, IonButtons, IonBackButton, IonCard, IonCardTitle, IonCardHeader, IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonCardContent, IonCardSubtitle, IonButton, IonButtons, IonBackButton, IonCard, IonCardTitle, IonCardHeader, IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-favourites',
   templateUrl: './favourites.page.html',
   styleUrls: ['./favourites.page.scss'],
   standalone: true,
-  imports: [IonButton, IonButtons, IonBackButton, IonCard, IonCardTitle, IonCardHeader, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonCardContent, IonCardSubtitle, IonButton, IonButtons, IonBackButton, IonCard, IonCardTitle, IonCardHeader, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class FavouritesPage implements OnInit {
 
